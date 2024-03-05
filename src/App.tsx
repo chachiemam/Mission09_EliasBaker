1ec5bddd92de6cc962c998b8e45f67a606d415f9
Eli Baker <ebaker42@byu.edu>
2024-03-04 18:10:49 -0700

    finished mission 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import collegeBasketballTeams from "./CollegeBasketballTeams.json";
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>D1 Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+
+        <br />
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {collegeBasketballTeams.teams.map((singleTeam) => (
+        <Team {...singleTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
